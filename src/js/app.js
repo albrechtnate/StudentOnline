0e12cb3f8d1e2ef94c7862ca8d64c3665654526f
photoguy2801 <nathanielalbrecht@gmail.com>
2014-11-24 06:30:31 -0500

    IndexedDB Work

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
 //@prepros-prepend vendor/*.js
 //@prepros-prepend foundation/*.js
 //@prepros-prepend js-modules/*.js
+
+var studentonline = {};
+studentonline.indexedDB = {};
+
+studentonline.indexedDB.db = null;
+
+studentonline.indexedDB.open = function() {
+	var version = 1;
+	var request = indexedDB.open("data", version);
+
+	request.onsuccess = function(e) {
+		studentonline.indexedDB.db = e.target.result;
+	};
+
+	request.onerror = studentonline.indexedDB.onerror;
+};

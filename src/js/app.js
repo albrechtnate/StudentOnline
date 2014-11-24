4cd8f555b483927d9d7a11b5d5845d667feb0f2e
photoguy2801 <nathanielalbrecht@gmail.com>
2014-11-24 10:45:53 -0500

    More IndexedDB work

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,72 @@
-//@prepros-prepend vendor/*.js
-//@prepros-prepend foundation/*.js
 //@prepros-prepend js-modules/*.js
 
-var studentonline = {};
-studentonline.indexedDB = {};
+// var studentonline = {};
+// studentonline.indexedDB = {};
 
-studentonline.indexedDB.db = null;
+// studentonline.indexedDB.db = null;
 
-studentonline.indexedDB.open = function() {
-	var version = 1;
-	var request = indexedDB.open("data", version);
+// studentonline.indexedDB.open = function() {
+// 	var version = 1;
+// 	var request = indexedDB.open("data", version);
 
-	request.onsuccess = function(e) {
-		studentonline.indexedDB.db = e.target.result;
-	};
+// 	request.onsuccess = function(e) {
+// 		studentonline.indexedDB.db = e.target.result;
+// 	};
 
-	request.onerror = studentonline.indexedDB.onerror;
-};
+// 	request.onerror = studentonline.indexedDB.onerror;
+// };
+
+var idbSupported = false;
+var db;
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    if("indexedDB" in window) {
+        idbSupported = true;
+    }
+
+    if(idbSupported) {
+        var openRequest = indexedDB.open("test", 6);
+
+        openRequest.onupgradeneeded = function(e) {
+            console.log("Upgrading...");
+            var thisDB = e.target.result;
+
+            if(!thisDB.objectStoreNames.contains("people")) {
+                thisDB.createObjectStore("people", { autoIncrement: true} );
+            }
+        }
+
+        openRequest.onsuccess = function(e) {
+            console.log("Success!");
+            db = e.target.result;
+
+            var transaction = db.transaction(["people"],"readwrite");
+            var store = transaction.objectStore("people");
+
+            var person = {
+            	name: "Nick",
+            	email: "google@email.com",
+            	created: new Date()
+            }
+
+            var request = store.add(person);
+
+            request.onsuccess = function(e) {
+            	console.log("Value Added to Database");
+            }
+
+            request.onerror = function(e) {
+            	console.log("Error adding value to database. ");
+            	console.dir(e);
+            }
+        }
+
+        openRequest.onerror = function(e) {
+            console.log("Error");
+            console.dir(e);
+        }
+
+    }
+
+},false);

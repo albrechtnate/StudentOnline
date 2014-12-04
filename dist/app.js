21d9837c2784382c15d440947fb55038dde913c8
photoguy2801 <nathanielalbrecht@gmail.com>
2014-12-04 09:17:37 -0500

    Changes with Processing

--- dist/app.js
+++
@@ -1,1 +0,0 @@
-function loadContent(t){xmlhttp=new XMLHttpRequest,xmlhttp.onreadystatechange=function(){4==xmlhttp.readyState&&200==xmlhttp.status&&(document.getElementById("content-section").innerHTML=xmlhttp.responseText)},xmlhttp.open("GET",t+".php",!0),xmlhttp.send()}loadContent("profile");

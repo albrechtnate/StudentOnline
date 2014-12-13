3fee80470333768bf3c2dfe442594855500d3e49
Nathaniel Albrecht <nathanielalbrecht@gmail.com>
2014-12-13 10:26:34 -0500

    Process template (.html) files into /build/ folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,21 +13,27 @@ gulp.task('connect', function() {
 });
 
 function reload() {
-	console.log("Reloading!!");
+	console.log("Reloading...");
 	return gulp.src('build/*.html')
 		.pipe(connect.reload());
 }
 
-// gulp.task('buildTemplates', function() {
-// 	gulp.src('app/*.html')
-// });
+gulp.task('buildTemplates', function() {
+	console.log("Processing Templates");
+	gulp.src('app/**/*.html')
+	.pipe(gulp.dest('build'))
+	.on('end', function() {
+		reload();
+	});
+});
 
 gulp.task('styles', function() {
+	console.log("Processing Styles");
 	return gulp.src('app/scss/*.scss')
 	// Hack: Turning off sourcemaps til gulp-ruby-sass goes to 1.0.0
 	// https://github.com/sindresorhus/gulp-autoprefixer/issues/20
 	// http://stackoverflow.com/questions/26979433/gulp-with-gulp-ruby-sass-error-style-css-map31-unknown-word
-	.pipe(sass({"sourcemap=none": true})).on('error', function(err){
+	.pipe(sass({"sourcemap=none": true, "quiet": true})).on('error', function(err){
 		console.log('******** SASS ERROR' + err);
 	})
 	.pipe(prefix())
@@ -38,9 +44,9 @@ gulp.task('styles', function() {
 	});
 });
 
-gulp.task('default', ['connect', 'styles'], function() {
+gulp.task('default', ['connect', 'buildTemplates', 'styles'], function() {
 	gulp.watch('app/scss/*.scss',['styles']);
-	gulp.watch('app/*.html', function(event){
-		reload();
-	});
+	gulp.watch('app/**/*.html', ['buildTemplates']);
+	console.log("\x1b[42mReady to Work\x1b[0m");
+	return reload();
 });

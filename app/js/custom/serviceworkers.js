dab1e0e50d775cee73cbb84e02fb29847a54edcd
photoguy2801 <nathanielalbrecht@gmail.com>
2015-05-07 09:14:05 -0400

    Changes

--- app/js/custom/serviceworkers.js
+++ app/js/custom/serviceworkers.js
@@ -1,12 +1,9 @@
 
-// ----- Offline Functionality: Functions That Use Service Workers to Create the Offline Experience ----- //
+// // ----- Offline Functionality: Functions That Use Service Workers to Create the Offline Experience ----- //
 
-// Establishes the Service Worker
-// navigator.serviceWorker.register('/sw.js').then(function(registration) {
-// 	// Registration was successful
-// 	console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-// }).catch(function(err) {
-// 	// registration failed :(
-// 	console.log('ServiceWorker registration failed: ', err);
+// // Establishes the Service Worker
+// navigator.serviceWorker.register('/worker.js').then(function(reg) {
+//   console.log('◕‿◕', reg);
+// }, function(err) {
+//   console.log('ಠ_ಠ', err);
 // });
-// }

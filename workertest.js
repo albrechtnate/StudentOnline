63653ad10ebbe60c96659474c2e52c83b9262443
photoguy2801 <nathanielalbrecht@gmail.com>
2015-05-08 06:53:32 -0400

    Worked on side test to examine service worker funtionality

---
+++ workertest.js
@@ -0,0 +1,27 @@
+console.log("SW startup");
+
+var CACHE_NAME = 'serviceworkertestcache';
+var urlsToCache = [
+	'/localhost:8080/fallback.html',
+];
+
+self.addEventListener('install', function(event) {
+	console.log("Installing...");
+
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	console.log("SW activated");
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log("Caught a fetch3!");
+	// caches.match('/fallback.html');
+});

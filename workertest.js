6e13232fee06d10d82cb4073b8729d273b67b9ad
photoguy2801 <nathanielalbrecht@gmail.com>
2015-05-11 06:28:11 -0400

    A little more work done on SW's

--- workertest.js
+++ workertest.js
@@ -1,27 +1,33 @@
-console.log("SW startup");
+console.log("SW on");
 
-var CACHE_NAME = 'serviceworkertestcache';
-var urlsToCache = [
-	'/localhost:8080/fallback.html',
-];
+// var CACHE_NAME = 'serviceworkertestcache';
+// var urlsToCache = [
+// 	'/localhost:8080/fallback.html',
+// ];
 
-self.addEventListener('install', function(event) {
-	console.log("Installing...");
+// self.addEventListener('install', function(event) {
+// 	console.log("Installing...");
 
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(function(cache) {
-				console.log('Opened cache');
-				return cache.addAll(urlsToCache);
-			})
-	);
-});
+// 	event.waitUntil(
+// 		caches.open(CACHE_NAME)
+// 			.then(function(cache) {
+// 				console.log('Opened cache');
+// 				// return cache.addAll(urlsToCache);
+// 			})
+// 	);
+// });
 
-self.addEventListener('activate', function(event) {
-	console.log("SW activated");
-});
+// self.addEventListener('activate', function(event) {
+// 	console.log("SW activated");
+// });
 
 self.addEventListener('fetch', function(event) {
 	console.log("Caught a fetch3!");
-	// caches.match('/fallback.html');
+
+	fetch(event.request).then(function(response) {
+		if (response.status >= 400) {
+			event.respondWith(new Response("Hello from your friendly neighbourhood service worker!"));
+		}
+	});
 });
+

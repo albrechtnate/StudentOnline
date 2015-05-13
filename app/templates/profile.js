cbf67c84e5c13846672dad398a4689ed0fdbc8e8
photoguy2801 <nathanielalbrecht@gmail.com>
2015-05-13 05:37:36 -0400

    Cleared branch and added test index.html

--- app/templates/profile.js
+++
@@ -1,9 +0,0 @@
-getDataFromIDB("student", "photo", "", function(result){
-	contentsection.append($(document.createElement('img')).attr("id", "classlist").attr("src", result));
-});
-
-getDataFromIDB("classes", "", "", function(result){
-	for (i in result){
-		$("#classschedule").append($(document.createElement('li')).text(result[i]["classname"]));
-	}
-})
--- notes/todo.md
+++
@@ -1,5 +0,0 @@
-# To Do
-
-- ~~Fix weird Foundation/Sass duplication/file size bug~~
-	- http://foundation.zurb.com/forum/posts/19668-styles-being-called-repeatedly-causing-delays--unresponsivness
-	- Try the node or libsass variant of Sass. **Switched to gulp-sass (port of LibSass). This fixes the issue.**

8ae469bfa2fe17759b7f011941bd42c8745a05e1
photoguy2801 <nathanielalbrecht@gmail.com>
2015-05-14 10:03:33 -0400

    Last minute changes

--- app/js/custom/interactionsounds.js
+++ app/js/custom/interactionsounds.js
@@ -1,10 +1,11 @@
 
 // ----- Interaction Sounds: Functions Relating to Any Auditory Feedback ----- //
 
+var audio = new Audio('resources/click.mp3');
+	audio.volume = 0.4;
+
 // Plays a Click Sound Whenever a Link or Button Is Pressed
 function addClickSound() {
-	var audio = new Audio('resources/click.mp3');
-	audio.volume = 0.4;
 
 	$('a, button').off('click.ClickSound');
 	$('a, button').on('click.ClickSound', function(){
--- app/js/custom/serviceworkers.js
+++
@@ -1,9 +0,0 @@
-
-// // ----- Offline Functionality: Functions That Use Service Workers to Create the Offline Experience ----- //
-
-// // Establishes the Service Worker
-// navigator.serviceWorker.register('/worker.js').then(function(reg) {
-//   console.log('◕‿◕', reg);
-// }, function(err) {
-//   console.log('ಠ_ಠ', err);
-// });
